b8db0ba554622e3ac5f8c746bee59e97829655ce
daniel mbuthia <danielwandurua@gmail.com>
2022-06-21 19:22:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+number_1 = int(two_digit_number[0])
+number_2 =int(two_digit_number[1])
+print( number_1 +number_2 )
+####################################
+#Write your code below this line 👇
+
+
+
+
+
+
+
